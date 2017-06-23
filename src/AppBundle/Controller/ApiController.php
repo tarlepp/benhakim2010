7025d6eb0674bac473ee6e52afd805995a28c190
tarlepp <tarmo.leppanen@protacon.com>
2017-06-23 18:28:46 +0300

    .

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiController extends Controller{
@@ -13,12 +15,46 @@ class ApiController extends Controller{
      */
     public function indexAction(SerializerInterface $serializer)
     {
-        $entity = $this->getDoctrine()
+        $users = $this->getDoctrine()
             ->getRepository('AppBundle:User')
             ->findAll();
 
-        $json = $serializer->serialize($entity,'json');
+        $json = $serializer->serialize($users,'json');
 
         return new JsonResponse($json, 200, [], true);
+    }
+
+    /**
+     * @Route("foo", name="api_foo")
+     */
+    public function fooAction(SerializerInterface $serializer)
+    {
+        $users = $this->getDoctrine()
+            ->getRepository('AppBundle:User')
+            ->findAll();
+
+        $json = $serializer->serialize($users,'json');
+
+        return new JsonResponse(['users' => json_decode($json)]);
+    }
+
+    /**
+     * @Route("bar", name="api_bar")
+     */
+    public function barAction(SerializerInterface $serializer)
+    {
+        $users = $this->getDoctrine()
+            ->getRepository('AppBundle:User')
+            ->findAll();
+
+        $normalizer = new ObjectNormalizer();
+
+        $iterator = function (User $user) use ($normalizer) {
+            return $normalizer->normalize($user);
+        };
+
+        $users = \array_map($iterator, $users);
+
+        return new JsonResponse(['users' => $users]);
     }
 }
